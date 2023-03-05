4da70e2ac3141ac9e0615f67a5b27ea817e1e2ff
vertexofvortex <tbm92ru@gmail.com>
2023-03-05 17:46:13 +0300

    ✨ Some features

--- components/CenterBlock/CenterBlock.tsx
+++ components/CenterBlock/CenterBlock.tsx
@@ -1,7 +1,18 @@
 import styles from "./CenterBlock.module.scss"; 
+import Image from "next/image";
 
 export function CenterBlock() {
   return (
-    <div>CenterBlock</div>
+    <div className={styles.centerBlock}>
+      <div className={styles.logo}>
+        <Image src={"/logo.svg"} alt={""} />
+      </div>
+      <div className={styles.description}>
+
+      </div>
+      <div className={styles.button}>
+
+      </div>
+    </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.scss'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter, Space_Grotesk } from "next/font/google";
+import styles from "@/styles/Home.module.scss";
+import { CenterBlock } from "@/components";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: [ "latin" ] });
+const space_grotesk = Space_Grotesk({ subsets: [ "latin" ] });
 
 export default function Home() {
   return (
     <>
-      
+      <CenterBlock />
     </>
   )
 }
