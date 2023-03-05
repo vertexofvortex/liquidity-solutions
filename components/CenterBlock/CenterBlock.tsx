017c8745d85060339607117b2b049f980be935ac
vertexofvortex <tbm92ru@gmail.com>
2023-03-05 21:15:16 +0300

    ✨ Center block, modal window

--- components/CenterBlock/CenterBlock.tsx
+++ components/CenterBlock/CenterBlock.tsx
@@ -1,26 +1,40 @@
 import styles from "./CenterBlock.module.scss"; 
-import Image from "next/image";
 import { Inter, Space_Grotesk } from "@next/font/google";
+import { ModalWindow } from "../ModalWindow/ModalWindow";
+import { useState } from "react";
 
 const inter = Inter({ subsets: [ "latin" ] });
 const space_grotesk = Space_Grotesk({ subsets: [ "latin" ] });
 
 export function CenterBlock() {
+  const [ isModalActive, setIsModalActive ] = useState<boolean>(false)
+
   return (
-    <div className={styles.centerBlock}>
-      <div className={styles.logo}>
-        <img src={"/logo.svg"} alt={""} />
+    <>
+      <div className={styles.centerBlock}>
+        <div className={styles.logo}>
+          <img src={"/logo.svg"} alt={""} />
+        </div>
+        <div className={`${styles.description} ${inter.className}`}>
+          <p>Our company is dedicated to delivering exceptional service and execution quality to our clients, leveraging our deep expertise and cutting-edge technology to help them achieve their trading goals. We work closely with our clients to understand their unique needs and develop tailored liquidity solutions that meet their specific requirements.</p>
+
+          <p>Our team of experienced professionals has a proven track record of success in the financial markets, and we are committed to staying at the forefront of industry developments and trends. We pride ourselves on our agility, responsiveness, and ability to innovate, and we are always striving to improve and enhance our offerings to better serve our clients.</p>
+        </div>
+        <div className={`${styles.button} ${space_grotesk.className}`}>
+          <button
+            onClick={() => setIsModalActive(true)}
+          >
+            Contact us
+          </button>
+        </div>
       </div>
-      <div className={`${styles.description} ${inter.className}`}>
-        <p>Our company is dedicated to delivering exceptional service and execution quality to our clients, leveraging our deep expertise and cutting-edge technology to help them achieve their trading goals. We work closely with our clients to understand their unique needs and develop tailored liquidity solutions that meet their specific requirements.</p>
-        
-        <p>Our team of experienced professionals has a proven track record of success in the financial markets, and we are committed to staying at the forefront of industry developments and trends. We pride ourselves on our agility, responsiveness, and ability to innovate, and we are always striving to improve and enhance our offerings to better serve our clients.</p>
-      </div>
-      <div className={`${styles.button} ${space_grotesk.className}`}>
-        <button>
-          Contact us
-        </button>
-      </div>
-    </div>
+      {
+        isModalActive && (
+          <ModalWindow
+            buttonAction={() => setIsModalActive(false)}
+          />
+        )
+      }
+    </>
   )
 }
--- components/ModalWindow/ModalWindow.tsx
+++ components/ModalWindow/ModalWindow.tsx
@@ -1,7 +1,38 @@
+import { Inter, Space_Grotesk } from "@next/font/google";
 import styles from "./ModalWindow.module.scss";
 
-export function ModalWindow() {
+const inter = Inter({ subsets: [ "latin" ] });
+const space_grotesk = Space_Grotesk({ subsets: [ "latin" ] });
+
+interface Props {
+  buttonAction: Function;
+}
+
+export function ModalWindow({
+  buttonAction,
+}: Props) {
   return (
-    <div>ModalWindow</div>
+    <div className={styles.modalWindowContainer}>
+      <div className={styles.modalWindow}>
+        <input 
+          className={inter.className}
+          placeholder={"Name"}
+        />
+        <input 
+          className={inter.className}
+          placeholder={"E-mail"}
+        />
+        <textarea rows={13}
+          className={inter.className}
+          placeholder={"Your message"}
+        />
+        <button
+          className={space_grotesk.className}
+          onClick={() => buttonAction()}
+        >
+          Send
+        </button>
+      </div>
+    </div>
   )
 }
