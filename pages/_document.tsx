1c48ae5ae583ec0444677e8f40a795ed17f18773
vertexofvortex <tbm92ru@gmail.com>
2023-03-07 15:51:58 +0300

    🔍 Added title property to head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Liquidity Solutions</title>
         <link rel="shortcut icon" href="/favicon.svg" />
 
         <meta property="og:type" content="website" />
